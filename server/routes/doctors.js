ad83e08247288fbb7aaeb77ec4abbeb2402c10d5
tandukar <84461389+tandukar@users.noreply.github.com>
2022-12-21 11:36:28 +0545

    adding authentication to doctors

---
+++ server/routes/doctors.js
@@ -0,0 +1,115 @@
+const router = require("express").Router();
+const Doctor = require('../model/Doctor');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { registerValidation, loginValidation } = require('../validation');
+
+
+router.post('/register', async(req, res) => {
+
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const emailExists = await Doctor.findOne({ email: req.body.email });
+    if (emailExists) return res.send("Email Already Exists");
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPwd = await bcrypt.hash(req.body.password, salt);
+
+    const postDoc = new Doctor({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        age: req.body.age,
+        email: req.body.email,
+        password: hashPwd,
+    })
+    try {
+        const savedDoc = await postDoc.save();
+        res.json(savedDoc);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+
+});
+
+
+//get all Doctors//
+router.get('/', async(req, res) => {
+    try {
+        const getDocs = await Doctor.find();
+        res.json(getDocs);
+    } catch (err) {
+        // res.status(400).send(err);
+        console.log(err);
+    }
+})
+
+
+//get one doctor
+router.get('/find/:doctorId', async(req, res) => {
+    try {
+        const findDoc = await Doctor.findById(req.params.doctorId);
+        res.json(findDoc);
+    } catch (err) {
+        res.status(400).send(err.message);
+
+    }
+})
+
+// update doctor 
+router.patch('/updateDoc/:doctorId', async(req, res) => {
+    try {
+        const updateDoc = await Doctor.updateMany({ _id: req.params.doctorId }, {
+            $set: {
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                password: req.body.password,
+                age: req.body.age,
+                email: req.body.email,
+            },
+        });
+        res.send(updateDoc);
+
+    } catch (err) {
+        res.json(err.message);
+    }
+})
+
+
+//delete a doctor
+router.delete('/deleteDoc/:doctorId', async(req, res) => {
+    try {
+        const delDoc = await Doctor.deleteOne({ _id: req.params.doctorId });
+        res.json(delDoc);
+    } catch (err) {
+        res.json(err.message)
+    }
+})
+
+
+
+//Login
+
+router.post('/login', async(req, res) => {
+
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //checking if email exists
+    const doctorLogin = await Doctor.findOne({ email: req.body.email });
+    if (!doctorLogin) return res.send("Email or Password is wrong");
+
+    const validPwd = await bcrypt.compare(req.body.password, doctorLogin.password);
+    if (!validPwd) return res.send("Email or Password is wrong");
+
+    //Create and assing token 
+    const token = jwt.sign({ _id: doctorLogin.id }, process.env.TOKEN);
+    res.header('auth-token', token).send(token);
+
+
+    res.send('login')
+
+
+})
+module.exports = router;
---
+++ server/validation.js
@@ -0,0 +1,29 @@
+const Joi = require("@hapi/joi");
+
+//Register Validation
+
+const registerValidation = (data) => {
+    const schema = Joi.object({
+        firstname: Joi.string().alphanum().min(1).max(100).required(),
+        lastname: Joi.string().alphanum().min(1).max(100).required(),
+        age: Joi.number().integer().min(12).max(130).required(),
+        email: Joi.string().required().email(),
+        password: Joi.string().alphanum().min(9).max(30).required(),
+    });
+
+    return schema.validate(data);
+
+};
+const loginValidation = (data) => {
+    const schema = Joi.object({
+
+        email: Joi.string().required().email(),
+        password: Joi.string().min(6).required(),
+    });
+
+    return schema.validate(data);
+};
+
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
