613b37a339f7cae4a0c4e84e1c561fceb86d06be
tandukar <84461389+tandukar@users.noreply.github.com>
2023-02-21 08:08:52 +0545

    testing

--- client/src/components/pages/Login/Login.jsx
+++ client/src/components/pages/Login/Login.jsx
@@ -30,8 +30,8 @@ const Login = () => {
                 <input
                   type="Email"
                   className="bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                  placeholder="Email"
                   value={email}
+                  placeholder="Email"
                   onChange={(event) => setEmail(event.target.value)}
                 />
               </div>
