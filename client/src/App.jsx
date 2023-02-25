03901416c9a1fc585b2e16f7b58628302526668d
tandukar <84461389+tandukar@users.noreply.github.com>
2023-02-25 21:25:12 +0545

    adding routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import AdminDashboard from './components/pages/adminDashboard/AdminDashboard';
 import Login from './components/pages/Login/Login';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div>
-      {/* <AdminDashboard /> */}
-      <Login />
-      {/* other components */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/navbar" element={<Navbar />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} /> */}
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+
+    {/* <AdminDashboard /> */}
+    {/* <Login /> */}
+    {/* other components */}
+  </div>
   );
 }
 
