caa80b16b97adaa7f17527f5a65052ad3def8d43
tandukar <84461389+tandukar@users.noreply.github.com>
2023-03-07 14:00:44 +0545

    securing authentication with token validation

--- client/src/components/pages/adminDashboard/AdminDashboard.jsx
+++ client/src/components/pages/adminDashboard/AdminDashboard.jsx
@@ -1,41 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Sidebar from "./sidebar/Sidebar";
 import DocDashboard from "./Doctor/DocDashboard";
 
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
 
+import { Navigate } from "react-router-dom";
+
 const AdminDashboard = () => {
   const [sidebarOpen, setSidebarOpen] = React.useState(false);
+  const [authenticated, setAuthenticated] = React.useState(null);
 
-  return (
-    <>
-      <div className="flex flex-col md:flex-row ">
-        {!sidebarOpen ? (
-          <div className="pt-4 pl-2 ">
-            <HiOutlineMenuAlt1
-              onClick={() => setSidebarOpen(true)}
-              className="text-gray-400 hover:text-blue-600 w-10 h-10 p-1 rounded "
-            />
-          </div>
-        ) : (
-          // <div className="w-60  ">
-          // <div className="fixed  z-50 md:relative md:w-60">
-            <div className="fixed md:relative md:w-60 h-full z-50  "> 
-            <Sidebar
-              sidebarOpen={sidebarOpen}
-              setSidebarOpen={setSidebarOpen}
-            />
-          </div>
-        )}
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      setAuthenticated(true);
+      console.log("token");
+    } else {
+      console.log("notoken");
+      setAuthenticated(false);
+    }
+  }, []);
 
-        <div className="flex-1  ">
-          <div className="min-height: 100vh ">
-            <DocDashboard />
+  if (authenticated === false) {
+    return <Navigate replace to="/login" />;
+  } else if (authenticated === true) {
+    console.log("authenticated");
+    return (
+      <>
+        <div className="flex flex-col md:flex-row ">
+          {!sidebarOpen ? (
+            <div className="pt-4 pl-2 ">
+              <HiOutlineMenuAlt1
+                onClick={() => setSidebarOpen(true)}
+                className="text-gray-400 hover:text-blue-600 w-10 h-10 p-1 rounded "
+              />
+            </div>
+          ) : (
+            <div className="fixed md:relative md:w-60 h-full z-50  ">
+              <Sidebar
+                sidebarOpen={sidebarOpen}
+                setSidebarOpen={setSidebarOpen}
+              />
+            </div>
+          )}
+          <div className="flex-1  ">
+            <div className="min-height: 100vh ">
+              <DocDashboard />
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } 
 };
+
 export default AdminDashboard;
