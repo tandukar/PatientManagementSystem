3500b542f76a45bd376d6daddee6b4f75be3e466
tandukar <84461389+tandukar@users.noreply.github.com>
2023-03-12 23:18:18 +0545

    connecting delete frontend and backend

--- client/src/components/pages/adminDashboard/Doctor/GetDoctors.jsx
+++ client/src/components/pages/adminDashboard/Doctor/GetDoctors.jsx
@@ -1,52 +1,92 @@
 import React from "react";
 import axios from "axios";
 
-export default class PersonList extends React.Component {
+// Delete confirmation pop-up component
+class DeleteConfirmation extends React.Component {
+  render() {
+    return (
+      <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 flex justify-center items-center">
+        <div className="bg-white rounded-xl p-8">
+          <p className="text-lg font-bold mb-4">Are you sure you want to delete this doctor?</p>
+          <div className="flex justify-end">
+            <button className="mr-4" onClick={this.props.onCancel}>Cancel</button>
+            <button className="bg-custom-blue text-white px-4 py-2 rounded-md" onClick={this.props.onConfirm}>Delete</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default class doctorList extends React.Component {
+  //creating an object of initial state
   state = {
-    persons: [],
+    doctors: [],
     id: "",
+    showDeleteConfirmation: false,
+    deleteDoctor: null,
   };
 
   componentDidMount() {
     axios.get(`http://localhost:5000/api/doctors/`).then((res) => {
-      const persons = res.data;
-      this.setState({ persons });
+      const doctors = res.data;
+      this.setState({ doctors });
     });
   }
 
+  // Set the id of the doctor to delete and show the delete confirmation pop-up
+  showDeleteConfirmation = (id) => {
+    this.setState({ deleteDoctor: id, showDeleteConfirmation: true });
+  }
+
+  // Hide the delete confirmation pop-up
+  hideDeleteConfirmation = () => {
+    this.setState({ showDeleteConfirmation: false });
+  }
+
+  // Delete the selected doctor
+  deleteDoctor = () => {
+    const id = this.state.deleteDoctor;
+    axios.delete(`http://localhost:5000/api/doctors/deleteDoc/${id}`)
+      .then(() => {
+        // Remove the deleted doctor from the list
+        const updateddoctors = this.state.doctors.filter((doctor) => doctor._id !== id);
+        this.setState({ doctors: updateddoctors, showDeleteConfirmation: false });
+      })
+      .catch((err) => console.log(err));
+  }
+
   render() {
-    const deleteDoctor = (id) => {
-      console.log('Delete doctor id:', id); // Check id value
-      axios.delete(`http://localhost:5000/api/doctors/deleteDoc/${id}`)
-        .then(res => {
-          console.log('Doctor deleted:', res.data);
-          // Update state or do other actions
-        })
-        .catch(err => console.log('Error deleting doctor:', err));
-    };
-    
     return (
-      <ul>
-        {this.state.persons.map((person) => (
-          <div className="p-1 ">
-            <li key={person.id}>
-              <div className="bg-white rounded-xl h-10 p-2 flex flex-row">
-                <div className="w-2/4 ml-5 font-bold text-custom-blue text-md">
-                  Dr. {person.firstname} {person.lastname}
+      <div>
+        <ul>
+          {this.state.doctors.map((doctor) => (
+            <div className="p-1 " key={doctor.id}>
+              <li>
+                <div className="bg-white rounded-xl h-10 p-2 flex flex-row">
+                  <div className="w-2/4 ml-5 font-bold text-custom-blue text-md">
+                    Dr. {doctor.firstname} {doctor.lastname}
+                  </div>
+                  <div className="w-1/4 font-bold text-gray-500 text-md text-end">
+                    Edit
+                  </div>
+                  <div className="w-1/4 font-bold text-red-700 text-md text-end mr-5">
+                    <button type="submit" onClick={() => this.showDeleteConfirmation(doctor._id)}>Delete</button>
+                  </div>
                 </div>
-                <div className="w-1/4 font-bold text-gray-500 text-md text-end">
-                  Edit
-                </div>
-                <div className="w-1/4 font-bold text-red-700 text-md text-end mr-5">
-                  <button type="submit" onClick={() => deleteDoctor(person._id)}>
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </li>
-          </div>
-        ))}
-      </ul>
+              </li>
+            </div>
+          ))}
+        </ul>
+
+        {/* Render the delete confirmation pop-up if it is shown */}
+        {this.state.showDeleteConfirmation && (
+          <DeleteConfirmation
+            onCancel={this.hideDeleteConfirmation}
+            onConfirm={this.deleteDoctor}
+          />
+        )}
+      </div>
     );
   }
 }
